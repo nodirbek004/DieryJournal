9f10a7146c69b2bc7d21e31013b2622339c08d18
nodirbek004 <allanazarovnodirshax@gmail.com>
2023-08-10 23:45:52 +0500

    Window File Ochdim

---
+++ DiaryJournal.Wiew/DiaryJournalWindow/SignUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryJournal.Wiew.DiaryJournalWindow
+{
+    internal class SignUp
+    {
+    }
+}
