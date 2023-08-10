9376738327a6f061035decc05ad25f4ba45116d4
nodirbek004 <allanazarovnodirshax@gmail.com>
2023-08-10 18:18:49 +0500

    modellarni Repasitorylarini yozdim

--- DiaryJournal.Data/Repasitories/UserRepasitory.cs
+++ DiaryJournal.Data/Repasitories/UserRepasitory.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DiaryJournal.Data.IReapasitories;
+using DiaryJournal.Data.Repasitories.Commons;
+using DiaryJournal.Domain.Entitys.Users;
 
-namespace DiaryJournal.Data.Repasitories
+namespace DiaryJournal.Data.Repasitories;
+
+public class UserRepasitory : Repasitory<User>, IUserRepasitory
 {
-    internal class UserRepasitory
+    public Task<User> GetByNumber(string number)
     {
+        throw new NotImplementedException();
     }
 }
